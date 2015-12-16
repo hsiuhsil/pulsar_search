64293e403511113a3d525ea7cd4c58fbd4d88bf7
Hsiu-Hsien Lin <hsiuhsil@gpc-f102n003-ib0.(none)>
2015-12-16 17:01:27 -0500

    make histogram for J2139+00

--- plot_histo.py
+++ plot_histo.py
@@ -3,18 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_histogram():
-
 f = h5py.File('/home/p/pen/hsiuhsil/pulsar_search/J2139+00h5', 'r')
 
+
 time = []
-time_interval = 1000
-bin = []
-for ii in xrange(len(f['ABS_TIME'])):
-    time += f['ABS_TIME'][ii]
+for ii in xrange(len(f['ABS_TIME'])-1):
+    time.append(f['ABS_TIME'][ii])
 
-for ii in xrange((max(time)-min(time))//100+1):
-    bin += [min(time)+ii*100]    
 
-plt.hist(time, bins=bin)
+print len(time) 
+
+plt.hist(time, bins=20)
+plt.title('J2139+00', fontsize=20)
+plt.xlabel('time(s)', fontsize=20)
+plt.ylabel('number of data', fontsize=20)
 plt.show()
