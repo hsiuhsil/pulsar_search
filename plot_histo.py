35c0f01a59b1c373a9038657d98a5753f7043886
Hsiu-Hsien Lin <hsiuhsil@gpc-f102n003-ib0.(none)>
2015-12-16 15:22:54 -0500

    make histogram

---
+++ plot_histo.py
@@ -0,0 +1,21 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_histogram():
+
+f = h5py.File('/home/p/pen/hsiuhsil/pulsar_search/J2139+00h5', 'r')
+
+time = []
+time_interval = 1000 
+bin = []
+for ii in xrange(len(f['ABS_TIME'])):
+    time += f['ABS_TIME'][ii]
+
+for ii in xrange((max(time)-min(time))//100+1):
+    bin += [min(time)+ii*100]    
+
+plt.hist(time, bins=bin)
+plt.show()
+
