a7e73bdec430fc8bea70b1f8424c83991907febb
Hsiu-Hsien Lin <hsiuhsil@gpc-f102n004-ib0.(none)>
2016-01-06 15:33:11 -0500

    making plots for number of data versus time

--- plot_histo.py
+++ plot_histo.py
@@ -1,20 +1,54 @@
+import sys
+import os.path
+
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
+def main():
+    args = sys.argv[1:]
+    for filename in args:
+        try:
+            plot_time(filename)
+        except (IOError, ValueError):
+            print 'Skipped:'+ filename
 
-f = h5py.File('/home/p/pen/hsiuhsil/pulsar_search/J2139+00h5', 'r')
+#f = h5py.File('/home/p/pen/hsiuhsil/pulsar_search/J2139+00h5', 'r')
 
+def plot_time(filename):
+    f = h5py.File(filename, 'r')
+    time_days = []
+    time_months = []
+    for ii in xrange(len(f['ABS_TIME'])-1):
+        time_days.append(f['ABS_TIME'][ii]/86400)
+#        print time_days[0]
+#        print time_days[-1]
+#        time_days[1:] = time_days[1:] - time_days[1]
+        time_months.append(f['ABS_TIME'][ii]/86400/30)
 
-time = []
-for ii in xrange(len(f['ABS_TIME'])-1):
-    time.append(f['ABS_TIME'][ii])
+    total_number = len(time_days)
+    file_name = f.filename[35:]
+    text = str(file_name) + ', total number: ' + str(total_number)
 
+    plt.figure(1)
+    plt.subplot(211)
+    plt.hist(time_days, bins=20)
+    plt.title(text, fontsize=20)
+    plt.xlabel('MJD', fontsize=20)
+    plt.ylabel('number of data', fontsize=20)
+    ax = plt.gca()
+    ax.ticklabel_format(useOffset=False)
+    plt.tick_params(axis='both', which='major', labelsize=20)
 
-print len(time) 
+    plt.subplot(212)
+    plt.hist(time_months, bins=20)
+    plt.xlabel('time(months)', fontsize=20)
+    plt.ylabel('number of data', fontsize=20)
+    plt.tick_params(axis='both', which='major', labelsize=20)
 
-plt.hist(time, bins=20)
-plt.title('J2139+00', fontsize=20)
-plt.xlabel('time(s)', fontsize=20)
-plt.ylabel('number of data', fontsize=20)
-plt.show()
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
