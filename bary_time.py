1ddaca00dda3a48a2e9feac4fc70e3ae8699a606
Hsiu-Hsien Lin <hsiuhsil@gpc-f102n002-ib0.(none)>
2016-05-16 14:30:17 -0400

    modify datatype information

--- bary_time.py
+++ bary_time.py
@@ -11,6 +11,7 @@ def main():
     args = sys.argv[1:]
     for filename in args:
         try:
+            print filename
             find_bary(filename)
         except (IOError, ValueError):
             print IOError
@@ -19,11 +20,9 @@ def main():
 def find_bary(filename):
 
     this_file = h5py.File(filename, "r+")
-    hduls = pyfits.open('/scratch2/p/pen/hsiuhsil/gbt_data/FRB110523/guppi_55704_wigglez22hrst_0258_0001.fits')
-    first_data = hduls[1].data[0]['OFFS_SUB']
+    first_data = this_file['ABS_TIME'][0]
     this_file.create_dataset('BARY_TIME', (0,) + first_data.shape, maxshape = (None,) +first_data.shape, dtype=first_data.dtype, chunks=True)
-    os.system('bary')
-#    print len(this_file['ABS_TIME'])
+
     for ii in range(len(this_file['ABS_TIME'])-1):
         RA = str(this_file['RA_SUB'][ii])
         DEC = str(this_file['DEC_SUB'][ii])
@@ -35,5 +34,5 @@ def find_bary(filename):
         this_file['BARY_TIME'][ii] = np.float64(bary_time)
   
   
-  if __name__ == '__main__':
+if __name__ == '__main__':
     main()
