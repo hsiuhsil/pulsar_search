514b8d0ddd2f76709c3d385dcdb6e9d979bca088
Hsiu-Hsien Lin <hsiuhsil@gpc-f102n001-ib0.(none)>
2016-06-01 11:33:47 -0400

    modify preprocess output shape

--- folding.py
+++ folding.py
@@ -30,6 +30,7 @@ def preprocessing(input_data):
 
     sigma_threshold = 5
     remove_period = 64
+    output_data = np.zeros(input_data.shape)
 
     data = input_data[:,0,:,0].T
     m = np.mean(data[:],axis=1)
@@ -40,7 +41,9 @@ def preprocessing(input_data):
     data = time_slope(data)
     preprocess.remove_noisy_freq(data, sigma_threshold)
     preprocess.remove_periodic(data, remove_period)
-    return data.T 
+    output_data[:,0,:,0] = data.T
+    output_data[:,1:4,:,0] = input_data[:,1:4,:,0]
+    return output_data
 
 def folding(filename):
     global this_file
@@ -51,13 +54,13 @@ def folding(filename):
     tbin = 0.001024
     do_preprocess = True   
 
-    first_data = this_file['DATA'][0]
-    data_folding = np.zeros((phase_bins,) + (first_data.shape[0], first_data.shape[2]))
+    first_data = this_file['DATA'][0][0]
+    data_folding = np.zeros((phase_bins,) + first_data.shape)
     
     '''collecting data which satisfies the folding condition'''
     same_modulo_num = [0]*phase_bins
-#    for ii in range(1):
-    for ii in range(len(this_file['BARY_TIME'])-1):
+    for ii in range(1):
+#   for ii in range(len(this_file['BARY_TIME'])-1):
         print 'ii = ' + str(ii)
         sample_BAT = this_file['BARY_TIME'][ii] + np.arange(-n_bins/2.0 + 0.5, n_bins/2.0 + 0.5)*tbin
         modulo_num = np.int64(np.around((sample_BAT % pulsar_period)/(pulsar_period/phase_bins)))
