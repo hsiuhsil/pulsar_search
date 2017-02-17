48bf6ca3be0412db2426edfb3b0946ca54ffc13d
Hsiu-Hsien Lin <hsiuhsil@gpc-f104n084-ib0.(none)>
2017-02-17 14:18:29 -0500

    randomize phase residuals

--- 2139_fit_timing.py
+++ 2139_fit_timing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import h5py
+import random
 import fit_timing
 import pars
 
@@ -12,6 +13,8 @@ def main():
 
 def plot_bary_diff():
 
+    '''Get timing profiles'''
+
     this_file_wz = pars.this_file_wz
     bin_number_wz = pars.bin_number_wz
     phase_amp_bin_wz = pars.phase_amp_bin_wz
@@ -23,9 +26,9 @@ def plot_bary_diff():
     time_mjd_wz, dBATdra_wz, dBATddec_wz, phase_data_wz, phase_data_err_wz = fit_timing.time_pattern(this_file_wz, bin_number_wz, phase_amp_bin_wz)
     time_mjd_1hr, dBATdra_1hr, dBATddec_1hr, phase_data_1hr, phase_data_err_1hr = fit_timing.time_pattern(this_file_1hr, bin_number_1hr, phase_amp_bin_1hr, pars.NPHASEBIN_1hr)
 
-#    print 'time_mjd_1hr',time_mjd_1hr
-#    print 'phase_data_1hr',phase_data_1hr
-#    print 'phase_data_err_1hr',phase_data_err_1hr
+    '''Randomize phase shift of residuals'''
+    np.random.shuffle(phase_data_err_wz)
+    np.random.shuffle(phase_data_err_1hr)
 
     time_mjd = np.concatenate((time_mjd_wz, time_mjd_1hr))
     dBATdra = np.concatenate((dBATdra_wz, dBATdra_1hr))
@@ -33,7 +36,7 @@ def plot_bary_diff():
     phase_data = np.concatenate((phase_data_wz, phase_data_1hr))
     phase_data_err = np.concatenate((phase_data_err_wz, phase_data_err_1hr))
 
-    
+    '''Find timing solution'''
     fit_timing.fitting(pars.fit_pars, time_mjd, dBATdra, dBATddec, phase_data, phase_data_err)
 
 
